178895342d9b26ca0fa0bdb3baa4c85667f43c2a
Mariana Mazzariello <mfmazzariello@gmail.com>
2021-11-19 23:33:14 +0100

    ADD js to increase big cup

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const smallCups = document.querySelectorAll(".cup-small");
-const liters = document.getElementById(".liters");
-const percentage = document.getElementById(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
+
+console.log(percentage, "hola");
+
+updateBigCup();
 
 smallCups.forEach(function (cup, idx) {
   // console.log(cup, idx);
@@ -24,4 +28,33 @@ function highlight(idx) {
       cup.classList.remove("full");
     }
   });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  console.log(fullCups);
+
+  const totalCups = smallCups.length;
+
+  console.log(totalCups);
+
+  if (fullCups === 0) {
+    console.log(percentage);
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
 }
